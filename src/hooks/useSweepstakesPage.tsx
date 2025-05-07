acaff6077b0726e075fb0108a697724531073e5e
sweepstouch957 <sweepstouchcloud@gmail.com>
2025-05-06 20:14:10 -0600

    bro me ayudas

--- src/hooks/useSweepstakesPage.tsx
+++ src/hooks/useSweepstakesPage.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import toast from "react-hot-toast";
